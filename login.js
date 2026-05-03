6d702d6ffa62e9ee1378f95b603310c361c0efa8
arpanelfranklin <arpanelfranklin@gmail.com>
2026-05-03 09:50:36 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 // LOCAL (testing)
-const API_BASE = "http://localhost:8080/api/auth";
+// const API_BASE = "http://localhost:8080/api/auth";
 
 // PRODUCTION
-// const API_BASE = "https://helpify-backend-iv27.onrender.com/api/auth";
+const API_BASE = "https://helpify-backend-iv27.onrender.com/api/auth";
 
 // ================= UI HELPERS =================
 function showMsg(text, type) {
